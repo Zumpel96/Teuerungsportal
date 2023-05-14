5931a806d26d93864d44f9abaeb84d5ee686bd47
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2023-05-14 14:42:06 +0200

    Added Service for Stores and the first API call

--- Teuerungsportal/Teuerungsportal/Pages/Stores.razor.cs
+++ Teuerungsportal/Teuerungsportal/Pages/Stores.razor.cs
@@ -4,19 +4,26 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using Teuerungsportal.Helpers;
 using Teuerungsportal.Resources;
+using Teuerungsportal.Services.Interfaces;
 
 public partial class Stores
 {
     [Inject]
     private IStringLocalizer<Language>? L { get; set; }
 
+    [Inject]
+    private StoreService? StoreService { get; set; }
+
     private ICollection<Store> StoresList { get; set; } = new List<Store>();
 
     /// <inheritdoc />
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
-        this.StoresList.Add(new Store() { Name = "Store A" });
-        this.StoresList.Add(new Store() { Name = "Store B" });
-        this.StoresList.Add(new Store() { Name = "Store C" });
+        if (this.StoreService == null)
+        {
+            return;
+        }
+
+        this.StoresList = await this.StoreService.GetStores();
     }
 }
--- Teuerungsportal/Teuerungsportal/Program.cs
+++ Teuerungsportal/Teuerungsportal/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Teuerungsportal;
 using Teuerungsportal.Resources;
+using Teuerungsportal.Services;
+using Teuerungsportal.Services.Interfaces;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,6 +16,7 @@ builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<StoreService, ApiStoreService>();
 
 var host = builder.Build();
 await host.SetDefaultCulture();
