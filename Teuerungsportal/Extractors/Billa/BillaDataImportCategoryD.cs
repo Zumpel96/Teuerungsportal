0bd922c1794003613ee1182b25c41b66ee4098b4
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2023-05-22 11:17:38 +0200

    Adjusted Extractor Timings and moved to separate functions app.

---
+++ Teuerungsportal/Extractors/Billa/BillaDataImportCategoryD.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Extractors.Billa;
+
+public static class BillaDataImportCategoryD
+{
+    [FunctionName("BillaDataImportCategoryD")]
+    public static async Task Run(
+        [TimerTrigger("0 0 4/12 * * *")] TimerInfo myTimer,
+        [Sql(commandText: "dbo.product", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Product> dbProducts,
+        [Sql(commandText: "dbo.price", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Price> dbPrices,
+        ILogger log)
+    {
+        log.LogInformation("Request received - Starting");
+        var url = $"https://shop.billa.at/api/search/full?category=B2-D&includeSort[]=rank&sort=rank&pageSize=1000000";
+        log.LogInformation("url: {Url}", url);
+
+        var dataExtractor = new BillaDataExtractor(url, dbProducts, dbPrices);
+        await dataExtractor.Run();
+    }
+}
