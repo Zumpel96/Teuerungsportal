908a452c1fa56093a5cd8105fa13f938503d8f5b
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2023-06-08 20:43:50 +0200

    Implemented DM Import

---
+++ Teuerungsportal/Extractors/DM/DmDataImportCategory5.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Extractors.Dm;
+
+using System;
+using Api.Extractors.DM;
+
+public static class DmDataImportCategory5
+{
+    [FunctionName("DmDataImportCategory5")]
+    public static async Task Run(
+        [TimerTrigger("0 30 7/12 * * *")] TimerInfo myTimer,
+        [Sql(commandText: "dbo.product", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Product> dbProducts,
+        [Sql(commandText: "dbo.price", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Price> dbPrices,
+        ILogger log)
+    {
+        log.LogInformation("Request received - Starting");
+        var url = $"https://product-search.services.dmtech.com/at/search/crawl?pageSize=10000&allCategories.id=050000";
+        log.LogInformation("url: {Url}", url);
+
+        var dataExtractor = new DmDataExtractor(url, dbProducts, dbPrices);
+        try
+        {
+            await dataExtractor.Run();
+        }
+        catch (Exception e)
+        {
+            log.LogInformation($"{e}");
+        }
+    }
+}
