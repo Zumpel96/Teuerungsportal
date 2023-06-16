57eb2d87d94588c331ec269fb9b9e2eb96ffb394
Zumpel96 <17179371+Zumpel96@users.noreply.github.com>
2023-06-16 14:24:55 +0200

    Added Hofer Data Extractor

---
+++ Teuerungsportal/Extractors/Hofer/HoferDataImportCategory11.cs
@@ -0,0 +1,30 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Extractors.Hofer;
+
+using System;
+
+public static class HoferDataImportCategory11
+{
+    [FunctionName("HoferDataImportCategory11")]
+    public static async Task Run(
+        [TimerTrigger("5 0 6/12 * * *")] TimerInfo myTimer,
+        [Sql(commandText: "dbo.product", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Product> dbProducts,
+        [Sql(commandText: "dbo.price", connectionStringSetting: "SqlConnectionString")] IAsyncCollector<Price> dbPrices,
+        ILogger log)
+    {
+        log.LogInformation("Request received - Starting");
+
+        var dataExtractor = new HoferDataExtractor("haushalt", dbProducts, dbPrices);
+        try
+        {
+            await dataExtractor.Run(log);
+        }
+        catch (Exception e)
+        {
+            log.LogInformation($"{e}");
+        }
+    }
+}
